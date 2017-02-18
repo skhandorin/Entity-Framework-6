5d444cd209f38495975040182d9d55f712f10f47
Сергей <@BLACK-PC>
2017-02-19 01:08:20 +0500

    Creating a Code Based Model and Database - Migrating a Database When Your Model Changes

--- Ninja.Module2Demo/NinjaDomain.Classes/Classes.cs
+++ Ninja.Module2Demo/NinjaDomain.Classes/Classes.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace NinjaDomain.Classes
 {
@@ -10,6 +11,7 @@ namespace NinjaDomain.Classes
         public Clan Clan { get; set; }
         public int ClanId { get; set; }
         public List<NinjaEquipment> EquipmentOwned { get; set; }
+        public System.DateTime DateOfBirth { get; set; }
     }
 
     public class Clan
@@ -24,6 +26,7 @@ namespace NinjaDomain.Classes
         public int Id { get; set; }
         public string Name { get; set; }
         public EquipmentType Type { get; set; }
+        [Required]
         public Ninja Ninja { get; set; }
     }
 }
